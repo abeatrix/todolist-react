1b0ad05cdf21dc7a422b01b71563d5f7fce337f6
Beatrix Woo <beatrixwho@gmail.com>
2020-10-21 08:54:34 -0700

    change config

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -2,21 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Routes from './config/routes';
 
-import Firebase from 'firebase';
-import config from './config';
-
-
-class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    Firebase.initializeApp(config.firebase);
-
-    this.state = {
-      developers: []
-    }
-  }
-
+function App() {
   return (
     <div className="container">
       <Header/>
