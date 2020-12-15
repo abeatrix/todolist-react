40ab9d83cd4d276b1b04d4c86da9de2fb34940fc
Beatrix Woo <beatrixwho@gmail.com>
2020-12-15 13:41:02 -0800

    Updates

---
+++ precache-manifest.f9d8eb154f78dac2e7f2cc2130da6b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da398fb4d02b84225f09d60b1ad58ee",
+    "url": "/abeatrix/todolist-react/index.html"
+  },
+  {
+    "revision": "ff5def0dd1bdcdc6376c",
+    "url": "/abeatrix/todolist-react/static/css/main.d9022057.chunk.css"
+  },
+  {
+    "revision": "1fd26091db8f9a7a8fd4",
+    "url": "/abeatrix/todolist-react/static/js/2.8e2a940b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/abeatrix/todolist-react/static/js/2.8e2a940b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5def0dd1bdcdc6376c",
+    "url": "/abeatrix/todolist-react/static/js/main.2d88745e.chunk.js"
+  },
+  {
+    "revision": "4838c2dba0986c4d290a",
+    "url": "/abeatrix/todolist-react/static/js/runtime-main.a0c18950.js"
+  }
+]);
